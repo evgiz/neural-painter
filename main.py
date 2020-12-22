86045081c7211fbdd924880a8a173895b5d6a5b0
sigve <me@evgiz.net>
2020-12-22 15:15:24 +0100

    Add paint target param

--- main.py
+++ main.py
@@ -121,6 +121,12 @@ if __name__ == "__main__":
         default=None,
         type=float
     )
+    paint.add_argument(
+        '--target', '-t',
+        required=False,
+        default="data/target_col.png",
+        type=str
+    )
 
     args = parser.parse_args(sys.argv[1:])
 
@@ -170,11 +176,11 @@ if __name__ == "__main__":
         model = NeuralPaintStroke(5)
         model.load_state_dict(torch.load(args.model, map_location=torch.device('cpu')))
 
-        target = cv2.imread('data/target_col.png', cv2.IMREAD_COLOR)
+        target = cv2.imread(args.target, cv2.IMREAD_COLOR)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
         target = torch.tensor([target / 255.0], dtype=torch.float)
         target = target.permute(0, 3, 1, 2)
-        print(target.shape)
+
         train.train_painting(
             target,
             model,
